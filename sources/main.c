b45c484acbc24577d006895e03cf851ad27a4170
Logan Farci <47923@etu.he2b.be>
2019-10-25 09:23:04 +0200

    status_information: pid parsing handles ERANGE errors and unvalid input

--- sources/main.c
+++ sources/main.c
@@ -3,9 +3,25 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <errno.h>
+#include <limits.h>
 
 #include "status_information_scanner.h"
 #include "awesomepsio.h"
+
+long parsePID(char *str)
+{
+    char *endptr;
+    long pid = strtol(str, &endptr, 10);
+    if (errno == ERANGE) {
+        printf("PID parsing error: the resulting value was out of range.\n");
+        exit(1);
+    }
+    if (endptr == str || *endptr != '\0') {
+        printf("PID parsing error: '%s' could not be parsed.\n", str);
+        exit(1);
+    }
+    return pid;
+}
 
 int main(int argc, char **argv)
 {
@@ -14,10 +30,8 @@ int main(int argc, char **argv)
         printf("usage: ps <pid>\n");
         return 1;
     }
-
-    int pid = strtol(argv[1], NULL, 10);
     status_information information;
-    scanStatusInformation(pid, &information);
+    scanStatusInformation(parsePID(argv[1]), &information);
     printFullStatusInformation(&information);
     return 0;
 }
