5ac45e7b6eec2459e19ce674920d3e63025e9463
Alexandre Baudot <42263@etu.he2b.be>
2019-11-01 20:39:11 +0100

    Find userName added

--- sources/user_information.c
+++ sources/user_information.c
@@ -72,8 +72,35 @@ int findProcessUserId(int pid)
     return userId;
 }
 
+
 char* findUserName(int userId)
 {
+    char * userName;
+    FILE * fp;
+    char * line = NULL ;
+    size_t length = 0
+    ssize_t read;
+    char *tokens[30];
+    bool found = FALSE ;
+    int i = 0;
     
+    fp = fopen("/etc/passwd","r");
+    if(fp==NULL) return(-1);
+    
+    while((read=getline(&line, &length, fp)) -= -1 && !found)
+    {
+        i=0
+        tokens[i]=strtok(line,":");
+        while (tokens[i] != NULL) tokens[++i]=strtok(NULL," ");
+        if((int)tokens[2] == userId)
+        {
+            found = TRUE;
+            userName = tokens[0];
+        }
+    }
+    fclose(fp);
+    if(line) free(line);
+    
+    return userName;
 }
 
