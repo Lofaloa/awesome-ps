63e6dd361007dab5d4bf9029e3a55bd5f14124ac
Logan Farci <47923@etu.he2b.be>
2019-11-16 22:07:48 +0100

    interaction: check for matching value supported

--- sources/interaction.c
+++ sources/interaction.c
@@ -15,16 +15,42 @@
  * format = *
  */
 
+static const char *keys[] = {"user", "status", "topic"};
+static const char *statusValues[] = {"running", "sleeping", "waiting", "zombie"};
+static const char *topicValues[] = {"paging", "general", "runtime"};
+
+static int array_contains(const char **array, unsigned count, char *value)
+{
+    for (unsigned i = 0; i < count; i++)
+    {
+        if (strcmp(array[i], value) == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 /* Tells if the given key is valid. A key is value when it is an available one
  * and is not null.
  */
 static int isValidKey(char *key)
 {
-    char *keys[] = {"user", "status", "format"};
-    unsigned numberOfKeys = sizeof(keys) / sizeof(keys[0]);
-    for (unsigned i = 0; i < numberOfKeys; i++) {
-        if (key != NULL && strcmp(keys[i], key) == 0) {
-            return 1;
+    return key != NULL && array_contains(keys, 3, key);
+}
+
+// Tells if the given value is valuee for the given key.
+static int isValidValueForKey(char *key, char *value)
+{
+    if (isValidKey(key))
+    {
+        if (strcmp("status", key) == 0)
+        {
+            return value != NULL && array_contains(statusValues, 4, value);
+        }
+        else if (strcmp("topic", key) == 0)
+        {            
+            return value != NULL && array_contains(topicValues, 3, value);
         }
     }
     return 0;
@@ -35,10 +61,25 @@ void setOptionFromString(char *str, awesomeps_option *option)
 {
     char *key = strtok(str, OPTION_SEPARATOR);
     char *value = strtok(NULL, OPTION_SEPARATOR);
-    if (isValidKey(key) && value != NULL) {
-        option->key = key;
-        option->value = value;
-    } else {
+    if (isValidKey(key))
+    {
+        if (isValidValueForKey(key, value))
+        {
+            option->key = key;
+            option->value = value;
+        }
+        else
+        {
+            printf(
+                "Option parsing error: %s isn't a valid value for %s.\n",
+                value, key
+            );
+            exit(-1);
+        }
+        
+    }
+    else
+    {
         printf("Option parsing error: %s is not a valid option\n", str);
         exit(-1);
     }
@@ -48,7 +89,8 @@ void readOptions(int argc, char **argv, awesomeps_option *options)
 {
     if (options != NULL)
     {
-        for (unsigned i = 1; i < argc; i++) {
+        for (unsigned i = 1; i < argc; i++)
+        {
             awesomeps_option option;
             setOptionFromString(argv[i], &option);
             options[i - 1] = option;
