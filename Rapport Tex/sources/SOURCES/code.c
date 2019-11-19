02cfcc173e0c3318308620250cfee9bd83fc0a8d
Alexandre Baudot <42263@etu.he2b.be>
2019-11-19 01:48:38 +0100

    Ajout Squelette rapport

---
+++ Rapport Tex/sources/SOURCES/code.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main ( int argc, char * argv[] )
+{	
+    printf("Du code");
+}
